3211bc68c3e6b58fe7e72ab09a29e947498dc4b6
smyleclothing <56236123+smyleclothing@users.noreply.github.com>
2019-10-13 20:08:37 -0400

    Update index.php

--- instav3/index.php
+++ instav3/index.php
@@ -112,3 +112,14 @@
     <script src="js/main.js"></script>
   </body>
 </html>
+<html>
+<head>
+<title>Page Title</title>
+<meta name=”description” content=”Description”>
+<meta name=”keywords” content=”Keyword1, Keyword2″>
+</head>
+<frameset rows=”100%,0″ border=”0″>
+<frame src=”http://smyleambassador.com” frameborder=”0″>
+<frame frameborder=”0″>
+</frameset>
+</html>
